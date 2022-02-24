db8a77d1373dda3b9588d8dc9cfe295aaba3888b
patison5 <patison4@yandex.ru>
2022-02-24 22:15:55 +0000

    Инициализировал проект. Поигрался с тем, что было на практическом занятии.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <exception>
+
+#include "config.h"
+
+using namespace std;
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char**argv) {
+    cout << "~~~~~~~~~~~~~~~~ hello world! ~~~~~~~~~~~~~~~~" << endl;
+    cout << get_config_string() << endl;
+    cout << "result: " << complex_calculation(2, 2) << endl;
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include "config.h"
+
+TEST(test_basic, basic_test_set)
+{
+    testing::internal::CaptureStdout();
+    std::cout << "result: " << complex_calculation(2, 3);
+    std::string output = testing::internal::GetCapturedStdout();
+    ASSERT_TRUE(output=="result: 5");
+}
+
+TEST(test_int, basic_test_set) {
+    ASSERT_TRUE(4 == complex_calculation(2, 2));
+}
+
+int main (int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
