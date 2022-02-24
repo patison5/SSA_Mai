106bd16030ad12baa549081d647f10f64b55c816
patison5 <patison4@yandex.ru>
2022-02-24 22:22:45 +0000

    навел порядок

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stud/src/SSA_Mai
-# Build directory: /home/stud/src/SSA_Mai/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(gtests "gtests")
-set_tests_properties(gtests PROPERTIES  _BACKTRACE_TRIPLES "/home/stud/src/SSA_Mai/CMakeLists.txt;31;add_test;/home/stud/src/SSA_Mai/CMakeLists.txt;0;")
---
+++ lab_00/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/stud/src/SSA_Mai/lab_00
+# Build directory: /home/stud/src/SSA_Mai/lab_00/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(gtests "gtests")
+set_tests_properties(gtests PROPERTIES  _BACKTRACE_TRIPLES "/home/stud/src/SSA_Mai/lab_00/CMakeLists.txt;31;add_test;/home/stud/src/SSA_Mai/lab_00/CMakeLists.txt;0;")
